b15aa4d2bfd5a71761aaa3927fb51ac37ef41fec
SANGEETHA VD <sangeetha2000.vd@gmail.com>
2024-11-14 16:28:05 +0530

    added a few headers

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
 def main(request):
